b3d50d0ad6f6c623445e55d575df7deb91b7ba06
Blair Wilcox <blairwilcox@Blairs-MacBook-Pro.local>
2020-05-16 22:28:01 -0400

    Add top navigation
    Adds a top navigation bar with a black background to the site. Had to
    copy the contents of header.php from Independent Publisher 2, but
    marked it accordingly

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+    add_action('wp_enqueue_scripts', 'enqueue_parent_styles');
+
+    function enqueue_parent_styles() {
+        wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    }
+
+    /**
+     * This function is modeled after independent_publisher_2_setup() in
+     * Independent Publisher 2's functions.php.
+     * 
+     * It just sets up various things
+     */
+    function independent_publisher_child_setup() {
+        register_nav_menus( array(
+            'top-menu' => esc_html__( 'Top Menu' )
+        ));
+    }
+    add_action( 'after_setup_theme', 'independent_publisher_child_setup' );
+?>
